18ccc2b254dad50641c5ad0cb68f2eb9731b0947
Peter Wagih Victor Zaky <peter.wagih90@gmail.com>
2019-01-29 16:26:30 +0200

    Udacity Log Analysis Project
    init project for review.
    add ReadMe file.
    add Project files.
    add Result file.

---
+++ README.md
@@ -0,0 +1,40 @@
+# Logs Analysis
+
+A Udacity FSND project.
+
+
+## Introduction
+
+This is the solution for the Logs Analysis project in Udacity Full Stack Nanodegree course.
+In this, we have to execute complex queries on a large database (> 1000k rows) to extract intersting stats.
+
+The database in question is a newspaper company database where we have 3 tables; `articles`, `authors` and `log`.
+* `articles` - Contains articles posted in the newspaper so far.
+* `authors` - Contains list of authors who have published their articles.
+* `log` - Stores log of every request sent to the newspaper server.
+
+This project implements a single query solution for each of the question in hand.
+See [logs-analysis-project.py](logs-analysis-project.py) for more details.
+
+## File location
+
+Keep in mind that if file wasn't executed on root `/` use `vagrant/logs-analysis-project`
+
+## Running
+
+* Make sure you have `newsdata.sql`, the SQL script file with all the data. It can be downloaded from the Udacity course page.
+
+* Then run the following command to execute it in `news` database. You might have to create the database before-hand.
+
+```sh
+psql -d news -f newsdata.sql
+```
+
+* Finally run the script.
+
+```sh
+python2 /vagrant/logs-analysis-project.py
+```
+
+* It will present you with necessary stats.
+Please see result file.
---
+++ logs-analysis-project.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+DATABASE_NAME = "news"
+
+# What are the most popular three articles of all time?
+def top3_articles():
+    print ("What are the most popular three articles of all time?")
+    output = [('Path', 'Count')]
+    db = psycopg2.connect(dbname=DATABASE_NAME)
+    cur = db.cursor()
+    # Query
+    cur.execute("create or replace view article_counting as select" +
+                    " path, count(*) as count from log group by path" +
+                    " order by count desc limit 3 offset 1;")
+    cur.execute("select a.title, c.count from articles a inner join" +
+                    " article_counting c on '/article/' || a.slug =" +
+                    " c.path order by c.count desc")
+    output += cur.fetchall()
+    db.close()
+
+    return output
+
+# Who are the most popular article authors of all time
+
+
+def top5_articles_writers():
+    print ("Who are the most popular article authors of all time?")
+    output = [('Path', 'Count')]
+    db = psycopg2.connect(dbname=DATABASE_NAME)
+    cur = db.cursor()
+    # Using join between tables to uses the stats from log table
+    cur.execute("select au.name, count(au.name) as count from authors au" +
+                    " inner join articles a on au.id = a.author inner join" +
+                    " log l on '/article/' || a.slug = l.path group by" +
+                    " au.name order by count desc limit 5;")
+    output += cur.fetchall()
+    db.close()
+
+    return output
+
+# On which days did more than 1% of requests lead to errors?
+
+
+def get_error_stats_by_day():
+
+    print ("On which days did more than 1% of requests lead to errors?")
+    output = [('Day', 'Error Percentage')]
+    db = psycopg2.connect(dbname=DATABASE_NAME)
+    cur = db.cursor()
+    # Creating a view to break the query
+    cur.execute("create or replace view error as select date(time)," +
+                    " count(*) as count from log where status =" +
+                    " '404 NOT FOUND' group by date(time) order by" +
+                    " count desc;")
+    cur.execute("create or replace view successful as select date(time)," +
+                    " count(*) as count from log where status ='200 OK' " +
+                    "  group by date(time) order by count desc;")
+    # Query to calculate the percentage
+    cur.execute("select e.date, round( (100 * cast(e.count as decimal) /" +
+                    " cast(s.count as decimal)),2) as percentage from " +
+                    " error e inner join successful s on e.date = s.date " +
+                    " order by e.count desc;")
+    output += cur.fetchall()
+    db.close()
+
+    return output
+
+# Function used to style the output: Option1 to
+# views and Option2 to Error
+
+
+def print_list(option, list):
+    if option == 'OPCAO1':
+        for x in range(len(list)):
+            print ("%s views  %s " % list[x])
+    elif option == 'OPCAO2':
+        for x in range(len(list)):
+            print ("%s - %s %% errors " % list[x])
+    else:
+        print ("Invalid: Option not found!")
+
+
+def main():
+
+    print
+    print_list('OPCAO1', top3_articles())
+    print
+    print_list('OPCAO1', top5_articles_writers())
+    print
+    print_list('OPCAO2', get_error_stats_by_day())
+    print
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
